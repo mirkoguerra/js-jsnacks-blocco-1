552110e8db981492e273352d132a3b5375d852d8
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-10 16:36:18 +0100

    jsnack-4

--- jsnack-4/js/script.js
+++ jsnack-4/js/script.js
@@ -1,4 +1,51 @@
 // mettendo tutto qui dentro, sono certo che il codice verrà eseguito quando il DOM è già stato caricato
 $(document).ready(function(){
 
+  // PARTE A
+  // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+  // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+  var nomeSquadraUno = prompt("inserire il nome della prima squadra");
+  var nomeSquadraDue = prompt("inserire il nome della seconda squadra");
+  var nomeSquadraTre = prompt("inserire il nome della terza squadra");
+
+  var squadre = [
+    {
+      nome: nomeSquadraUno,
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+    {
+      nome: nomeSquadraDue,
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+    {
+      nome: nomeSquadraTre,
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
+  ];
+
+  // console.log(squadre);
+
+  //PARTE B
+  // Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti
+
+  function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  for (var i=0; i<squadre.length; i++) {
+    squadre[i]["puntiFatti"] = getRandomInt(0, 50)
+    squadre[i]["falliSubiti"] = getRandomInt(0, 100)
+    $(".squadre").append("nome: " + squadre[i]["nome"] + "<br>");
+    $(".squadre").append("punti fatti: " + squadre[i]["puntiFatti"] + "<br>");
+    $(".squadre").append("falli subiti: " + squadre[i]["falliSubiti"] + "<br>");
+  }
+
+  // console.log(squadre);
+
 });
